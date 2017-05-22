3dee7143a38ba2a8c634353b01d9a80ba568018b
ahmed abdelfatah <giniyat202@yahoo.com>
2017-05-23 01:05:08 +0200

    lit cube

--- main.cpp
+++ main.cpp
@@ -29,6 +29,7 @@ bool checkall()
     checksingle(ARB_vertex_shader);
     checksingle(ARB_fragment_shader);
     checksingle(ARB_multisample);
+    fflush(stderr);
     return true;
 }
 
@@ -56,47 +57,56 @@ void init()
     struct customv
     {
         float pos[3];
-        float col[3];
+        float norm[3];
     };
 
-    float angx[] = {0.0f, 0.0f, 0.0f, 0.0f, 90.0f, 270.0f};
-    float angy[] = {0.0f, 90.0f, 180.0f, 270.0f, 0.0f, 0.0f};
-    float cols[][3] = {
-        { 1.0f, 0.0f, 0.0f, },
-        { 0.0f, 1.0f, 0.0f, },
-        { 0.0f, 0.0f, 1.0f, },
-        { 1.0f, 1.0f, 0.0f, },
-        { 1.0f, 0.0f, 1.0f, },
-        { 0.0f, 1.0f, 1.0f, },
+    static const float angx[] = {0.0f, 0.0f, 0.0f, 0.0f, 90.0f, 270.0f};
+    static const float angy[] = {0.0f, 90.0f, 180.0f, 270.0f, 0.0f, 0.0f};
+    static const vec3 norms[] = {
+        vec3(0.0f, 0.0f, 1.0f),
+        vec3(1.0f, 0.0f, 0.0f),
+        vec3(0.0f, 0.0f, -1.0f),
+        vec3(-1.0f, 0.0f, 0.0f),
+        vec3(0.0f, -1.0f, 0.0f),
+        vec3(0.0f, 1.0f, 0.0f),
     };
 
-    customv vertex[24];
-    unsigned int index[36];
-    vec4 face[] = {
-        vec4(0.5f, 0.5f, 0.5f, 1.0f),
-        vec4(-0.5f, 0.5f, 0.5f, 1.0f),
-        vec4(-0.5f, -0.5f, 0.5f, 1.0f),
-        vec4(0.5f, -0.5f, 0.5f, 1.0f),
+    customv vertex[] = {
+        {  0.5f,  0.5f,  0.5f, 0.0f, 0.0f, 0.0f },
+        { -0.5f,  0.5f,  0.5f, 0.0f, 0.0f, 0.0f },
+        { -0.5f, -0.5f,  0.5f, 0.0f, 0.0f, 0.0f },
+        {  0.5f, -0.5f,  0.5f, 0.0f, 0.0f, 0.0f },
+        {  0.5f,  0.5f, -0.5f, 0.0f, 0.0f, 0.0f },
+        { -0.5f,  0.5f, -0.5f, 0.0f, 0.0f, 0.0f },
+        { -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f },
+        {  0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f },
+    };
+    unsigned int index[] = {
+        0, 1, 2,
+        2, 3, 0,
+
+        4, 0, 3,
+        3, 7, 4,
+
+        5, 4, 7,
+        7, 6, 5,
+
+        1, 5, 6,
+        6, 2, 1,
+
+        3, 2, 6,
+        6, 7, 3,
+
+        4, 5, 1,
+        1, 0, 4,
     };
 
     for (int i = 0; i < 6; i++)
     {
-
-        mat4 rotx = rotate(mat4(1.0f), radians(angx[i]), vec3(1.0f, 0.0f, 0.0f));
-        mat4 roty = rotate(mat4(1.0f), radians(angy[i]), vec3(0.0f, 1.0f, 0.0f));
-        for (int j = 0; j < 4; j++)
-        {
-            vec4 tr = roty * rotx * face[j];
-            for (int k = 0; k < 3; k++)
-            {
-                vertex[i * 4 + j].pos[k] = tr[k];
-            }
-            memcpy(vertex[i * 4 + j].col, cols[i], sizeof(cols[i]));
-        }
-        unsigned int ind[] = { 0, 1, 2, 2, 3, 0 };
         for (int j = 0; j < 6; j++)
         {
-            index[i * 6 + j] = i * 4 + ind[j];
+            for (int k = 0; k < 3; k++)
+                vertex[index[i * 6 + j]].norm[k] += norms[i][k];
         }
     }
     glGenVertexArrays(1, &VAO);
@@ -107,13 +117,29 @@ void init()
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_STATIC_DRAW);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+
     unsigned int position = glGetAttribLocation(prog.id(), "position");
-    unsigned int color = glGetAttribLocation(prog.id(), "color");
+    unsigned int normal = glGetAttribLocation(prog.id(), "normal");
     glEnableVertexAttribArray(position);
     glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, sizeof(customv), 0);
-    glEnableVertexAttribArray(color);
-    glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, sizeof(customv), (void*)12);
+    glEnableVertexAttribArray(normal);
+    glVertexAttribPointer(normal, 3, GL_FLOAT, GL_FALSE, sizeof(customv), (void*)12);
     glBindVertexArray(0);
+
+    prog.use();
+    vec3 light_position(0.0f, 0.0f, 0.0f);
+    vec3 light_ambient(0.2f, 0.2f, 0.2f);
+    vec3 light_diffuse(0.8f, 0.8f, 0.8f);
+    vec3 light_specular(0.8f, 0.8f, 0.8f);
+    unsigned int light_position_id = glGetUniformLocation(prog.id(), "light.position");
+    unsigned int light_ambient_id = glGetUniformLocation(prog.id(), "light.ambient");
+    unsigned int light_diffuse_id = glGetUniformLocation(prog.id(), "light.diffuse");
+    unsigned int light_specular_id = glGetUniformLocation(prog.id(), "light.specular");
+    glUniform3fv(light_position_id, 1, &light_position[0]);
+    glUniform3fv(light_ambient_id, 1, &light_ambient[0]);
+    glUniform3fv(light_diffuse_id, 1, &light_diffuse[0]);
+    glUniform3fv(light_specular_id, 1, &light_specular[0]);
+    prog.unuse();
 }
 
 float angle;
@@ -127,30 +153,41 @@ void timer(int)
 void display()
 {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-           glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-           // Draw our first triangle
-           prog.use();
-           GLuint id = glGetUniformLocation(prog.id(), "mvp");
-           //Matrix projection = Matrix::perspective(70.0f, 4.0f / 3.0f, 1.0f, 100.0f);
-           //Matrix projection = Matrix::ortho(-10.0, 10.0, -10.0, 10.0, -1.0, 1.0f);
-           //Matrix vp = projection * Matrix::translate(0.0f, 0.0f, -7.0f);
-           //Matrix model = Matrix::rotate(angle, 0.0f, 1.0f, 0.0f) ;//*
-                   //Matrix::scale(2.0f, 2.0f, 2.0f);
-           //Matrix mvp = vp * model;
-           glm::mat4 projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 1.0f, 100.0f);
-           glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -7.0f));
-           glm::mat4 model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(1.0f, 1.0f, 0.0f));
-           glm::mat4 mvp = projection * view * model;
+    // Draw our first triangle
+    prog.use();
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 1.0f, 100.0f);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -7.0f));
+    glm::mat4 model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(1.0f, 1.0f, 0.0f));
+    glm::mat4 modelview = view * model;
+    mat3 timodel = mat3(transpose(inverse(modelview)));
+    vec3 material_ambient(0.0f, 0.0f, 0.0f);
+    vec3 material_diffuse(0.5f, 0.0f, 0.0f);
+    vec3 material_specular(0.7f, 0.6f, 0.6f);
+    float material_shininess = 0.25f * 128.0f;
 
-           glUniformMatrix4fv(id, 1, GL_FALSE, &mvp[0][0]);
-           glBindVertexArray(VAO);
-           //glDrawArrays(GL_TRIANGLES, 0, 6);
-           glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, (void*)(0 * 4));
-           Program::unuse();
-           glBindVertexArray(0);
-           glFlush();
-           glutSwapBuffers();
+    unsigned int modelview_id = glGetUniformLocation(prog.id(), "modelview");
+    unsigned int projection_id = glGetUniformLocation(prog.id(), "projection");
+    unsigned int timodel_id = glGetUniformLocation(prog.id(), "timodel");
+    unsigned int material_ambient_id = glGetUniformLocation(prog.id(), "material.ambient");
+    unsigned int material_diffuse_id = glGetUniformLocation(prog.id(), "material.diffuse");
+    unsigned int material_specular_id = glGetUniformLocation(prog.id(), "material.specular");
+    unsigned int material_shininess_id = glGetUniformLocation(prog.id(), "material.shininess");
+    glUniformMatrix4fv(modelview_id, 1, GL_FALSE, &modelview[0][0]);
+    glUniformMatrix4fv(projection_id, 1, GL_FALSE, &projection[0][0]);
+    glUniformMatrix3fv(timodel_id, 1, GL_FALSE, &timodel[0][0]);
+    glUniform3fv(material_ambient_id, 1, &material_ambient[0]);
+    glUniform3fv(material_diffuse_id, 1, &material_diffuse[0]);
+    glUniform3fv(material_specular_id, 1, &material_specular[0]);
+    glUniform1fv(material_shininess_id, 1, &material_shininess);
+
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, (void*)(0));
+    Program::unuse();
+    glBindVertexArray(0);
+    glFlush();
+    glutSwapBuffers();
 }
 
 int main(int argc, char **argv)
---
+++ test.vert
@@ -0,0 +1,18 @@
+#version 120
+attribute vec3 position;
+attribute vec3 normal;
+
+varying vec3 FragPos;
+varying vec3 Normal;
+
+uniform mat4 modelview;
+uniform mat4 projection;
+uniform mat3 timodel;
+void main()
+{
+    Normal = timodel * normal;
+    vec4 pos = vec4(position, 1.0);
+    FragPos = vec3(pos.x, pos.y, pos.z - 7.0);
+    //FragPos = vec3(modelview * pos);
+    gl_Position = projection * modelview * pos;
+}
